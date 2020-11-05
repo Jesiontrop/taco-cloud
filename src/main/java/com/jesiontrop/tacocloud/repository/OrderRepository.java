94bebb2e421906b86f28b952f263f7ab94183e82
Jusepa <43047614+Jusepa@users.noreply.github.com>
2020-11-05 21:41:00 +0300

    Added function in repository

--- src/main/java/com/jesiontrop/tacocloud/repository/OrderRepository.java
+++ src/main/java/com/jesiontrop/tacocloud/repository/OrderRepository.java
@@ -1,7 +1,14 @@
 package com.jesiontrop.tacocloud.repository;
 
 import com.jesiontrop.tacocloud.model.Order;
+import com.jesiontrop.tacocloud.model.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends CrudRepository<Order, Long> {
+
+    List<Order> findByUserOrderByPlacedAtDesc(User user);
+    List<Order> findByUserOrderByPlacedAtDesc(User user, Pageable pageable);
 }
