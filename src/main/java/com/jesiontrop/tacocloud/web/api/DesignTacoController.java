ea4cf824ec991fe97a55a63b8029097ef4d9f4cc
Jusepa <43047614+Jusepa@users.noreply.github.com>
2020-12-15 23:08:26 +0300

    Create DesignTacoController.java

---
+++ src/main/java/com/jesiontrop/tacocloud/web/api/DesignTacoController.java
@@ -0,0 +1,20 @@
+package com.jesiontrop.tacocloud.web.api;
+
+import com.jesiontrop.tacocloud.model.Taco;
+import com.jesiontrop.tacocloud.repository.TacoRepository;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "/design",
+    produces="application/json")
+@CrossOrigin(origins = "*")
+public class DesignTacoController {
+}
+
