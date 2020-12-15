1166eca3417a74b7fd3ea985f5a8e7d3f5b95689
Jusepa <43047614+Jusepa@users.noreply.github.com>
2020-12-15 20:34:21 +0300

    Change CrudRepository to PagingAndSortingRepository

--- src/main/java/com/jesiontrop/tacocloud/repository/TacoRepository.java
+++ src/main/java/com/jesiontrop/tacocloud/repository/TacoRepository.java
@@ -1,7 +1,10 @@
 package com.jesiontrop.tacocloud.repository;
 
 import com.jesiontrop.tacocloud.model.Taco;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface TacoRepository extends CrudRepository<Taco, Long> {
+public interface TacoRepository extends PagingAndSortingRepository<Taco, Long> {
+
 }
