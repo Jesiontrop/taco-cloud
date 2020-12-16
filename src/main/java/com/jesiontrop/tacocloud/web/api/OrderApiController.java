3902496382b4931e048a5661534aef420222d99a
Jusepa <43047614+Jusepa@users.noreply.github.com>
2020-12-16 21:46:20 +0300

    Add Order Repository

--- src/main/java/com/jesiontrop/tacocloud/web/api/OrderApiController.java
+++ src/main/java/com/jesiontrop/tacocloud/web/api/OrderApiController.java
@@ -10,4 +10,10 @@ import org.springframework.web.bind.annotation.*;
                 produces = "application/json")
 @CrossOrigin(origins = "*")
 public class OrderApiController {
+    private OrderRepository orderRepository;
+
+    public OrderApiController(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
 }
