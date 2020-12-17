5b0718521e8e9066348a8abf498115adad2a99c7
Jusepa <43047614+Jusepa@users.noreply.github.com>
2020-12-18 00:33:31 +0300

    Update TacoModel.java

--- src/main/java/com/jesiontrop/tacocloud/web/api/TacoModel.java
+++ src/main/java/com/jesiontrop/tacocloud/web/api/TacoModel.java
@@ -1,3 +1,27 @@
 package com.jesiontrop.tacocloud.web.api;
+
+import com.jesiontrop.tacocloud.model.Ingredient;
+import com.jesiontrop.tacocloud.model.Taco;
+import lombok.Getter;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.util.Date;
+import java.util.List;
+
 public class TacoModel extends RepresentationModel {
+
+    @Getter
+    private final String name;
+
+    @Getter
+    private final Date createdAt;
+
+    @Getter
+    private final List<Ingredient> ingredients;
+
+    public TacoModel(Taco taco) {
+        this.name = taco.getName();
+        this.createdAt = taco.getCreatedAt();
+        this.ingredients = taco.getIngredients();
+    }
 }
