33b64b34a691c0243ad40b189f5774556e4def15
Jusepa <43047614+Jusepa@users.noreply.github.com>
2020-12-17 20:07:30 +0300

    Change name and add WebMvcLinkBuilder

--- src/main/java/com/jesiontrop/tacocloud/web/api/DesignTacoApiController.java
+++ src/main/java/com/jesiontrop/tacocloud/web/api/DesignTacoApiController.java
@@ -5,28 +5,40 @@ import com.jesiontrop.tacocloud.repository.TacoRepository;
 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/design",
     produces="application/json")
 @CrossOrigin(origins = "*")
-public class DesignTacoController {
+public class DesignTacoApiController {
     private TacoRepository tacoRepository;
 
-    public DesignTacoController(TacoRepository tacoRepository) {
+    public DesignTacoApiController(TacoRepository tacoRepository) {
         this.tacoRepository = tacoRepository;
     }
 
     @GetMapping("/recent")
-    public Iterable<Taco> recentTaco() {
+    public CollectionModel<EntityModel<Taco>> recentTaco() {
         PageRequest page = PageRequest.of(
             0, 12, Sort.by("createdAt").descending());
-        return tacoRepository.findAll(page).getContent();
+
+        List<Taco> tacos = tacoRepository.findAll(page).getContent();
+        CollectionModel<EntityModel<Taco>> collectionModel = CollectionModel.wrap(tacos);
+
+        collectionModel.add(
+            WebMvcLinkBuilder.linkTo(DesignTacoApiController.class)
+                            .slash("recent")
+                            .withRel("recents");
+        return collectionModel;
     }
 
     @GetMapping("/{id}")
