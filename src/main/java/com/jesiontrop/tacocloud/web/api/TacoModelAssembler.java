d925e917cca940693b1b61cff64f727dbf5b10d4
Jusepa <43047614+Jusepa@users.noreply.github.com>
2020-12-18 22:09:10 +0300

    Update TacoModelAssembler.java

--- src/main/java/com/jesiontrop/tacocloud/web/api/TacoModelAssembler.java
+++ src/main/java/com/jesiontrop/tacocloud/web/api/TacoModelAssembler.java
@@ -1,4 +1,22 @@
 package com.jesiontrop.tacocloud.web.api;
 
-public class TacoModelAssembler {
+import com.jesiontrop.tacocloud.model.Taco;
+import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
+
+public class TacoModelAssembler
+            extends RepresentationModelAssemblerSupport<Taco, TacoModel> {
+
+    public TacoModelAssembler(Class<?> controllerClass) {
+        super(controllerClass, TacoModel.class);
+    }
+
+    @Override
+    protected TacoModel instantiateModel(Taco entity) {
+        return new TacoModel(entity);
+    }
+
+    @Override
+    public TacoModel toModel(Taco entity) {
+        return createModelWithId(entity.getId(), entity);
+    }
 }
