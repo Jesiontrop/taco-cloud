17a393b80a46440f9be6bd8b4e536e6cdc7aeb2e
Jusepa <43047614+Jusepa@users.noreply.github.com>
2020-12-19 01:36:41 +0300

    Add IngredientAssembler into class

--- src/main/java/com/jesiontrop/tacocloud/web/api/TacoModel.java
+++ src/main/java/com/jesiontrop/tacocloud/web/api/TacoModel.java
@@ -1,14 +1,18 @@
 package com.jesiontrop.tacocloud.web.api;
 
-import com.jesiontrop.tacocloud.model.Ingredient;
 import com.jesiontrop.tacocloud.model.Taco;
 import lombok.Getter;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.RepresentationModel;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 public class TacoModel extends RepresentationModel<TacoModel> {
+
+    private static final IngredientModelAssembler ingredientAssembler =
+        new IngredientModelAssembler(IngredientApiController.class);
 
     @Getter
     private final String name;
@@ -17,11 +21,11 @@ public class TacoModel extends RepresentationModel<TacoModel> {
     private final Date createdAt;
 
     @Getter
-    private final List<Ingredient> ingredients;
+    private final List<IngredientModel> ingredients;
 
     public TacoModel(Taco taco) {
         this.name = taco.getName();
         this.createdAt = taco.getCreatedAt();
-        this.ingredients = taco.getIngredients();
+        this.ingredients = new ArrayList<>(ingredientAssembler.toCollectionModel(taco.getIngredients()).getContent());
     }
 }
