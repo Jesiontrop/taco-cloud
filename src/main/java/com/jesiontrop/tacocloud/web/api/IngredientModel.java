02d0454dffc47280c9205e8a09c59018928955a7
Jusepa <43047614+Jusepa@users.noreply.github.com>
2020-12-19 00:06:58 +0300

    Update IngredientModel.java

--- src/main/java/com/jesiontrop/tacocloud/web/api/IngredientModel.java
+++ src/main/java/com/jesiontrop/tacocloud/web/api/IngredientModel.java
@@ -1,4 +1,20 @@
 package com.jesiontrop.tacocloud.web.api;
+
+import com.jesiontrop.tacocloud.model.Ingredient;
+import com.jesiontrop.tacocloud.model.Ingredient.Type;
+import lombok.Getter;
 import org.springframework.hateoas.RepresentationModel;
+
 public class IngredientModel extends RepresentationModel<IngredientModel> {
+
+    @Getter
+    private String name;
+
+    @Getter
+    private Type type;
+
+    public IngredientModel (Ingredient ingredient) {
+        this.name = ingredient.getName();
+        this.type = ingredient.getType();
+    }
 }
