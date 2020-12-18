91dbc8537738ab03b8c1277f0beb06068befe09e
Jusepa <43047614+Jusepa@users.noreply.github.com>
2020-12-19 01:35:29 +0300

    Add IngredientRepository

--- src/main/java/com/jesiontrop/tacocloud/web/api/IngredientApiController.java
+++ src/main/java/com/jesiontrop/tacocloud/web/api/IngredientApiController.java
@@ -1,12 +1,27 @@
 package com.jesiontrop.tacocloud.web.api;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.jesiontrop.tacocloud.model.Ingredient;
+import com.jesiontrop.tacocloud.repository.IngredientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/ingredients",
                 produces = "application/json")
 @CrossOrigin(origins = "*")
 public class IngredientApiController {
+
+    IngredientRepository ingredientRepository;
+
+    @Autowired
+    public IngredientApiController(IngredientRepository ingredientRepository) {
+        this.ingredientRepository = ingredientRepository;
+    }
+
 }
