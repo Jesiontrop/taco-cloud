eb8a378988109578c88c19c81b82f20b7423dd22
Jusepa <43047614+Jusepa@users.noreply.github.com>
2020-12-23 22:02:37 +0300

    Add RepresentationMdelProcessor to Taco Entity

--- src/main/java/com/jesiontrop/tacocloud/web/api/SpringDataRestConfiguration.java
+++ src/main/java/com/jesiontrop/tacocloud/web/api/SpringDataRestConfiguration.java
@@ -1,7 +1,30 @@
 package com.jesiontrop.tacocloud.web.api;
 
+import com.jesiontrop.tacocloud.model.Taco;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.hateoas.server.EntityLinks;
+import org.springframework.hateoas.server.RepresentationModelProcessor;
 
 @Configuration
 public class SpringDataRestConfiguration {
+
+    @Bean
+    public RepresentationModelProcessor<PagedModel<EntityModel<Taco>>> tacoProcessor(EntityLinks entityLinks) {
+
+        return new RepresentationModelProcessor<PagedModel<EntityModel<Taco>>>() {
+            @Override
+            public PagedModel<EntityModel<Taco>> process(PagedModel<EntityModel<Taco>> model) {
+
+                model.add(
+                    entityLinks.linkFor(Taco.class)
+                        .slash("recent")
+                        .withRel("recents"));
+
+                return model;
+            }
+        };
+    }
 }
