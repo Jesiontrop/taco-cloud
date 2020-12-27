380f60a3db5883390825f35a0d742110a146eb74
Jusepa <43047614+Jusepa@users.noreply.github.com>
2020-12-27 20:41:27 +0300

    Add RestTemplate and constructor with him

--- src/main/java/com/jesiontrop/tacocloud/restclient/TacoCloudClient.java
+++ src/main/java/com/jesiontrop/tacocloud/restclient/TacoCloudClient.java
@@ -2,7 +2,15 @@ package com.jesiontrop.tacocloud.restclient;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
 @Service
 @Slf4j
 public class TacoCloudClient {
+
+    private RestTemplate restTemplate;
+
+    public TacoCloudClient(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 }
