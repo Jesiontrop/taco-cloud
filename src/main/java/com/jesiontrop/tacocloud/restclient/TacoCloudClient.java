db4367c3158926d5c3b24729878b535accbf1366
Jusepa <43047614+Jusepa@users.noreply.github.com>
2020-12-27 20:43:28 +0300

    Add getIngredientById method
    He receives (String ingredientId)

--- src/main/java/com/jesiontrop/tacocloud/restclient/TacoCloudClient.java
+++ src/main/java/com/jesiontrop/tacocloud/restclient/TacoCloudClient.java
@@ -1,5 +1,6 @@
 package com.jesiontrop.tacocloud.restclient;
 
+import com.jesiontrop.tacocloud.model.Ingredient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -12,5 +13,10 @@ public class TacoCloudClient {
 
     public TacoCloudClient(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
+    }
+
+    public Ingredient getIngredientById(String ingredientId) {
+        return restTemplate.getForObject("http://localhost:8080/ingredients/{id}",
+                                            Ingredient.class, ingredientId);
     }
 }
