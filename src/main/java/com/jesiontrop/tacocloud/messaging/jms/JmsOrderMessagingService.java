4623ef40b0179f0573793f66b0d4fc8b1df42a54
Jusepa <43047614+Jusepa@users.noreply.github.com>
2021-01-08 13:39:12 +0300

    Add anotation @Service

--- src/main/java/com/jesiontrop/tacocloud/messaging/jms/JmsOrderMessagingService.java
+++ src/main/java/com/jesiontrop/tacocloud/messaging/jms/JmsOrderMessagingService.java
@@ -2,8 +2,10 @@ package com.jesiontrop.tacocloud.messaging.jms;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Service;
 
 public class JmsOrderMessagingService {
+@Service
 
     private JmsTemplate jmsTemplate;
 
