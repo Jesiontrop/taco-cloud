98a1d316797cba88e7cc795aaf0a684cdc3eeb76
Jusepa <43047614+Jusepa@users.noreply.github.com>
2021-01-08 13:39:49 +0300

    Implement OrderMessagingService with method sendOrder(Order order)

--- src/main/java/com/jesiontrop/tacocloud/messaging/jms/JmsOrderMessagingService.java
+++ src/main/java/com/jesiontrop/tacocloud/messaging/jms/JmsOrderMessagingService.java
@@ -1,16 +1,32 @@
 package com.jesiontrop.tacocloud.messaging.jms;
 
+import com.jesiontrop.tacocloud.model.Order;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 import org.springframework.stereotype.Service;
 
-public class JmsOrderMessagingService {
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
+
 @Service
+public class JmsOrderMessagingService implements OrderMessagingService{
 
     private JmsTemplate jmsTemplate;
 
     @Autowired
     public JmsOrderMessagingService(JmsTemplate jmsTemplate) {
         this.jmsTemplate = jmsTemplate;
+    }
+
+    @Override
+    public void sendOrder(Order order) {
+        jmsTemplate.send(new MessageCreator() {
+            @Override
+            public Message createMessage(Session session) throws JMSException {
+                return session.createObjectMessage(order);
+            }
+        });
     }
 }
