f41b0026a1317e5ed278f139661da8b48dad9472
Jusepa <43047614+Jusepa@users.noreply.github.com>
2021-01-08 13:30:56 +0300

    Add JmsTemplate

--- src/main/java/com/jesiontrop/tacocloud/messaging/jms/JmsOrderMessagingService.java
+++ src/main/java/com/jesiontrop/tacocloud/messaging/jms/JmsOrderMessagingService.java
@@ -1,4 +1,14 @@
 package com.jesiontrop.tacocloud.messaging.jms;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+
 public class JmsOrderMessagingService {
+
+    private JmsTemplate jmsTemplate;
+
+    @Autowired
+    public JmsOrderMessagingService(JmsTemplate jmsTemplate) {
+        this.jmsTemplate = jmsTemplate;
+    }
 }
