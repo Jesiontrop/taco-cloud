0e9ee3ea7f045fc4a51043fec04c9a960ed8b97d
Jusepa <43047614+Jusepa@users.noreply.github.com>
2021-01-12 23:16:29 +0300

    Add Bean messageConverter()

--- src/main/java/com/jesiontrop/tacocloud/messaging/jms/MessagingConfig.java
+++ src/main/java/com/jesiontrop/tacocloud/messaging/jms/MessagingConfig.java
@@ -3,6 +3,7 @@ package com.jesiontrop.tacocloud.messaging.jms;
 import org.apache.activemq.artemis.jms.client.ActiveMQQueue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 
 import javax.jms.Destination;
 
@@ -12,5 +13,13 @@ public class MessagingConfig {
     @Bean
     public Destination orderQueue() {
         return new ActiveMQQueue("taco.order.queue");
+    }
+
+    @Bean
+    public MappingJackson2MessageConverter messageConverter() {
+        MappingJackson2MessageConverter messageConverter =
+                                        new MappingJackson2MessageConverter();
+        messageConverter.setTypeIdPropertyName("_typeId");
+        return messageConverter;
     }
 }
