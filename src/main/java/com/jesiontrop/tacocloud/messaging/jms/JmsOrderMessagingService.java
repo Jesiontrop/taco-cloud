5946072e751fedc4d6cdf06fde43c07e7838b820
Jusepa <43047614+Jusepa@users.noreply.github.com>
2021-01-13 20:01:27 +0300

    Add addOrderSource(Message message)

--- src/main/java/com/jesiontrop/tacocloud/messaging/jms/JmsOrderMessagingService.java
+++ src/main/java/com/jesiontrop/tacocloud/messaging/jms/JmsOrderMessagingService.java
@@ -25,6 +25,11 @@ public class JmsOrderMessagingService implements OrderMessagingService{
 
     @Override
     public void sendOrder(Order order) {
-        jmsTemplate.convertAndSend(orderQueue, order);
+        jmsTemplate.convertAndSend(orderQueue, order, this::addOrderSource);
+    }
+
+    private Message addOrderSource(Message message) throws JMSException {
+        message.setStringProperty("X_ORDER_SOURCE", "WEB");
+        return message;
     }
 }
