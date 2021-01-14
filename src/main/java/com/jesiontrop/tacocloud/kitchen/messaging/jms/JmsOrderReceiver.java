a4e2940d93f98dabac32fe561e53b8d5c654d4ee
Jusepa <43047614+Jusepa@users.noreply.github.com>
2021-01-14 22:41:18 +0300

    Add JmsTemplate

--- src/main/java/com/jesiontrop/tacocloud/kitchen/messaging/jms/JmsOrderReceiver.java
+++ src/main/java/com/jesiontrop/tacocloud/kitchen/messaging/jms/JmsOrderReceiver.java
@@ -1,8 +1,9 @@
 package com.jesiontrop.tacocloud.kitchen.messaging.jms;
 
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JmsOrderReceiver {
-
+    private JmsTemplate jmsTemplate;
 }
