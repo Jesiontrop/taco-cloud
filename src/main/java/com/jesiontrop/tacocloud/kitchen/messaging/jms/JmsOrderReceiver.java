7c869c36ec3b5c3c3c02324577860ed111678588
Jusepa <43047614+Jusepa@users.noreply.github.com>
2021-01-14 22:41:36 +0300

    Add MessageConverter

--- src/main/java/com/jesiontrop/tacocloud/kitchen/messaging/jms/JmsOrderReceiver.java
+++ src/main/java/com/jesiontrop/tacocloud/kitchen/messaging/jms/JmsOrderReceiver.java
@@ -1,9 +1,11 @@
 package com.jesiontrop.tacocloud.kitchen.messaging.jms;
 
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JmsOrderReceiver {
     private JmsTemplate jmsTemplate;
+    private MessageConverter messageConverter;
 }
