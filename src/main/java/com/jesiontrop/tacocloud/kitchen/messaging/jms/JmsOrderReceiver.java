26304c6f4bc965e08b86a77ccc7e83b0e5c25115
Jusepa <43047614+Jusepa@users.noreply.github.com>
2021-01-14 22:52:59 +0300

    Add orderQueue property

--- src/main/java/com/jesiontrop/tacocloud/kitchen/messaging/jms/JmsOrderReceiver.java
+++ src/main/java/com/jesiontrop/tacocloud/kitchen/messaging/jms/JmsOrderReceiver.java
@@ -6,10 +6,13 @@ import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.stereotype.Component;
 
+import javax.jms.Destination;
+
 @Component
 public class JmsOrderReceiver implements OrderReceiver{
     private JmsTemplate jmsTemplate;
     private MessageConverter messageConverter;
+    private Destination orderQueue;
 
     @Autowired
     public JmsOrderReceiver(JmsTemplate jmsTemplate, MessageConverter messageConverter) {
