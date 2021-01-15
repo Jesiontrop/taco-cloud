2d17e98714c475f31a11fc40283f66de6b00bdcd
Jusepa <43047614+Jusepa@users.noreply.github.com>
2021-01-15 19:01:46 +0300

    Create KitchenUI.java

---
+++ src/main/java/com/jesiontrop/tacocloud/kitchen/messaging/jms/KitchenUI.java
@@ -0,0 +1,9 @@
+package com.jesiontrop.tacocloud.kitchen.messaging.jms;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class KitchenUI {
+}
