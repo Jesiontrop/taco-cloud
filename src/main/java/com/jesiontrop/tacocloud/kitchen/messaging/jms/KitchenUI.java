720e9b4675f943a7fc2dae198b52ce882a2b959c
Jusepa <43047614+Jusepa@users.noreply.github.com>
2021-01-15 19:02:56 +0300

    Add displayOrder

--- src/main/java/com/jesiontrop/tacocloud/kitchen/messaging/jms/KitchenUI.java
+++ src/main/java/com/jesiontrop/tacocloud/kitchen/messaging/jms/KitchenUI.java
@@ -1,9 +1,14 @@
 package com.jesiontrop.tacocloud.kitchen.messaging.jms;
 
+import com.jesiontrop.tacocloud.model.Order;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
 public class KitchenUI {
+
+    public void displayOrder(Order order) {
+        log.info("RECEIVED ORDER: " + order);
+    }
 }
