67baf6628934793dc08633a573426b835f003b42
Jusepa <43047614+Jusepa@users.noreply.github.com>
2021-01-16 17:02:09 +0300

    Add kitchenUi and orderQueue property

--- src/main/java/com/jesiontrop/tacocloud/kitchen/messaging/jms/listener/OrderListener.java
+++ src/main/java/com/jesiontrop/tacocloud/kitchen/messaging/jms/listener/OrderListener.java
@@ -1,7 +1,13 @@
 package com.jesiontrop.tacocloud.kitchen.messaging.jms.listener;
 
+import com.jesiontrop.tacocloud.kitchen.messaging.jms.KitchenUI;
 import org.springframework.stereotype.Component;
+
+import javax.jms.Destination;
 
 @Component
 public class OrderListener {
+    private KitchenUI kitchenUI;
+    private Destination orderQueue;
+
 }
