e08f0fb4579bcbd32c7e7431350cd75a39b88da3
Jusepa <43047614+Jusepa@users.noreply.github.com>
2021-01-16 17:02:40 +0300

    Add Autowired constructor

--- src/main/java/com/jesiontrop/tacocloud/kitchen/messaging/jms/listener/OrderListener.java
+++ src/main/java/com/jesiontrop/tacocloud/kitchen/messaging/jms/listener/OrderListener.java
@@ -1,6 +1,7 @@
 package com.jesiontrop.tacocloud.kitchen.messaging.jms.listener;
 
 import com.jesiontrop.tacocloud.kitchen.messaging.jms.KitchenUI;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.jms.Destination;
@@ -10,4 +11,9 @@ public class OrderListener {
     private KitchenUI kitchenUI;
     private Destination orderQueue;
 
+    @Autowired
+    public OrderListener(KitchenUI kitchenUI, Destination orderQueue) {
+        this.kitchenUI = kitchenUI;
+        this.orderQueue = orderQueue;
+    }
 }
