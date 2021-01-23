7d2236e30806da8458a70e7014bd55df2c428183
Jusepa <43047614+Jusepa@users.noreply.github.com>
2021-01-23 12:57:40 +0300

    Implement sendOrder(Order order) method

--- src/main/java/com/jesiontrop/tacocloud/messaging/rabbitmq/RabbitOrderMessagingService.java
+++ src/main/java/com/jesiontrop/tacocloud/messaging/rabbitmq/RabbitOrderMessagingService.java
@@ -1,7 +1,10 @@
 package com.jesiontrop.tacocloud.messaging.rabbitmq;
 
 import com.jesiontrop.tacocloud.model.Order;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +19,9 @@ public class RabbitOrderMessagingService implements OrderMessagingService{
 
     @Override
     public void sendOrder(Order order) {
-
+        MessageConverter messageConverter = rabbitTemplate.getMessageConverter();
+        MessageProperties messageProperties = new MessageProperties();
+        Message message = messageConverter.toMessage(order, messageProperties);
+        rabbitTemplate.send("tacocloud.order" ,message);
     }
 }
