436697d4620add1fee4db76bd304879d5697f9d6
Jusepa <43047614+Jusepa@users.noreply.github.com>
2021-01-23 12:40:47 +0300

    Add RabbitTemplate property

--- src/main/java/com/jesiontrop/tacocloud/messaging/rabbitmq/RabbitOrderMessagingService.java
+++ src/main/java/com/jesiontrop/tacocloud/messaging/rabbitmq/RabbitOrderMessagingService.java
@@ -1,9 +1,19 @@
 package com.jesiontrop.tacocloud.messaging.rabbitmq;
 
+import com.jesiontrop.tacocloud.model.Order;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RabbitOrderMessagingService implements OrderMessagingService{
+    private RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    public RabbitOrderMessagingService(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
     @Override
     public void sendOrder(Order order) {
 
