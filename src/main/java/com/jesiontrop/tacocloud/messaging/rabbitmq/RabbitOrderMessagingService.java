ef423d99e38e0131320541aab6d1628a7f54c885
Jusepa <43047614+Jusepa@users.noreply.github.com>
2021-01-23 12:40:27 +0300

    Implement OrderMessaginService

--- src/main/java/com/jesiontrop/tacocloud/messaging/rabbitmq/RabbitOrderMessagingService.java
+++ src/main/java/com/jesiontrop/tacocloud/messaging/rabbitmq/RabbitOrderMessagingService.java
@@ -3,5 +3,9 @@ package com.jesiontrop.tacocloud.messaging.rabbitmq;
 import org.springframework.stereotype.Service;
 
 @Service
-public class RabbitOrderMessagingService {
+public class RabbitOrderMessagingService implements OrderMessagingService{
+    @Override
+    public void sendOrder(Order order) {
+
+    }
 }
